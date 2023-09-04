72caaa019154f2819812269dfa1c620ca407d512
JiriRedeker <jiri.redeker@gmail.com>
2023-09-04 12:00:50 +0200

    eerste ding met customtkinter, beetje de basis van een scherm maken

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import customtkinter as ctk
+
+
 ### Variables ### 
 
 ### Functies ###
 
 def main():
-    pass
+    print("Hello world!")
+    window()
+
+
+def window():
+    root = ctk.CTk()
+    root.geometry("1920x1080")
+    ctk.set_appearance_mode("dark")
+    root.mainloop()
+    window = ctk.CTkFrame(master=root)
+    window.pack(pady=0, padx=0, fill="both", expand=True)
+
+
+
+
 
 if __name__ == "__main__":
     main()
