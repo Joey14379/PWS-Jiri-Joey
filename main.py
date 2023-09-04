c3774366ac623d44fb716ee68e41198875baffaa
JiriRedeker <143389430+JiriRedeker@users.noreply.github.com>
2023-09-04 12:08:04 +0200

    Revert "eerste ding met customtkinter, beetje de basis van een scherm maken"

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-import customtkinter as ctk
-
-
 ### Variables ### 
 
 ### Functies ###
 
 def main():
-    print("Hello world!")
-    window()
-
-
-def window():
-    root = ctk.CTk()
-    root.geometry("1920x1080")
-    ctk.set_appearance_mode("dark")
-    root.mainloop()
-    window = ctk.CTkFrame(master=root)
-    window.pack(pady=0, padx=0, fill="both", expand=True)
-
-
-
-
+    pass
 
 if __name__ == "__main__":
     main()
